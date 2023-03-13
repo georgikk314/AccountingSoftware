be377ffca992210befa4634612044c1b81985c5a
georgikk314 <gogomen2005@gmail.com>
2023-03-14 00:20:41 +0200

    Made the design for user authentication

---
+++ UserAuthentication.xaml.cs
@@ -0,0 +1,14 @@
+namespace AccountingSoftware;
+
+public partial class UserAuthentication : ContentPage
+{
+	public UserAuthentication()
+	{
+		InitializeComponent();
+	}
+
+    private void LoginButton_Clicked(object sender, EventArgs e)
+	{
+
+	}
+}
