2182c9cf1f95326a361d3088088cdf59c5fdd025
georgikk314 <gogomen2005@gmail.com>
2023-03-15 00:33:12 +0200

    Made the Inventory Management design. It is not fully done, because it needs an information from the database, which is not set up yet!

---
+++ InventoryManagement.xaml.cs
@@ -0,0 +1,19 @@
+namespace AccountingSoftware;
+
+public partial class InventoryManagement : ContentPage
+{
+	public InventoryManagement()
+	{
+		InitializeComponent();
+	}
+
+    private void OnAddItemClicked(object sender, EventArgs e)
+    {
+        //TODO:
+    }
+
+    private void OnDownloadPdfClicked(object sender, EventArgs e)
+    {
+        //TODO:
+    }
+}
