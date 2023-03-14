3404a372fb31218aa7597a503ee4fcfe9f207b05
georgikk314 <gogomen2005@gmail.com>
2023-03-14 22:57:34 +0200

    Made the record transactions design

---
+++ RecordTransactions.xaml.cs
@@ -0,0 +1,14 @@
+namespace AccountingSoftware;
+
+public partial class RecordTransactions : ContentPage
+{
+	public RecordTransactions()
+	{
+		InitializeComponent();
+	}
+
+    private void OnSaveTransactionClicked(object sender, EventArgs e)
+    {
+
+    }
+}
