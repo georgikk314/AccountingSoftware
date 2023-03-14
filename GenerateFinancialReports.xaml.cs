dfba92fd3d8e229f48d8fe7fd13e5e555784b336
georgikk314 <gogomen2005@gmail.com>
2023-03-15 00:08:26 +0200

    Made the design for generate financial reports window

---
+++ GenerateFinancialReports.xaml.cs
@@ -0,0 +1,24 @@
+namespace AccountingSoftware;
+
+public partial class GenerateFinancialReports : ContentPage
+{
+	public GenerateFinancialReports()
+	{
+		InitializeComponent();
+	}
+
+    private void OnGenerateReportClicked(object sender, EventArgs e)
+    {
+        //TODO:
+    }
+
+    private void OnDownloadPDFClicked(object sender, EventArgs e)
+    {
+        //TODO:
+    }
+
+    private void ReportTypePicker_Unfocused(object sender, FocusEventArgs e)
+    {
+
+    }
+}
