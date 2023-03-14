b5d46a1ae88b31563637e2e38586a8ab3d78c700
georgikk314 <gogomen2005@gmail.com>
2023-03-15 00:19:49 +0200

    Made the Client Management window design

---
+++ ClientManagement.xaml.cs
@@ -0,0 +1,14 @@
+namespace AccountingSoftware;
+
+public partial class ClientManagement : ContentPage
+{
+	public ClientManagement()
+	{
+		InitializeComponent();
+	}
+
+    private void OnSaveClientClicked(object sender, EventArgs e)
+    {
+        //TODO:
+    }
+}
