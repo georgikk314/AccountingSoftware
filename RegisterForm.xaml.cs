bb6b17e49a2a57707ce35099ef7027b933ff2185
georgikk314 <gogomen2005@gmail.com>
2023-03-15 23:57:48 +0200

    Made the design for the register form

---
+++ RegisterForm.xaml.cs
@@ -0,0 +1,14 @@
+namespace AccountingSoftware;
+
+public partial class RegisterForm : ContentPage
+{
+	public RegisterForm()
+	{
+		InitializeComponent();
+	}
+
+    private void OnRegisterClicked(object sender, EventArgs e)
+    {
+        //TODO:
+    }
+}
