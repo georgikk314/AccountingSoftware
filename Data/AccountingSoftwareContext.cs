c9065f7c5b57f8bcdd557d1ae2da691f5606cb8d
georgikk314 <gogomen2005@gmail.com>
2023-03-17 13:00:15 +0200

    created the dbcontext file and repaired a well-known bug in maui app when installing EF

---
+++ Data/AccountingSoftwareContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountingSoftware.Data
+{
+    public class AccountingSoftwareContext : DbContext
+    {
+        
+        public AccountingSoftwareContext()
+        {
+            SQLitePCL.Batteries_V2.Init();
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "AccountingSoftware.db3");
+
+            optionsBuilder
+                .UseSqlite($"Filename={dbPath}");
+        }
+        
+    }
+}
