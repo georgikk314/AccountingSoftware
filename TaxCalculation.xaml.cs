7c52afda7821df0a243e161e069b7298f8648a4d
gladiaTORA <rado0511@abv.bg>
2023-03-19 12:54:34 +0200

    add tax calculation design

---
+++ TaxCalculation.xaml.cs
@@ -0,0 +1,13 @@
+namespace AccountingSoftware;
+
+public partial class TaxCalculation : ContentPage
+{
+	public TaxCalculation()
+	{
+		InitializeComponent();
+	}
+    private void OnCalculateTaxClicked(object sender, EventArgs e)
+    {
+        // Tax calculation code here
+    }
+}
